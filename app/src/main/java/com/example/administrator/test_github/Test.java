2d296c4b5dbc096d56df1235f8d1c819ee98c445
liuye326 <liuye326@sina.com>
2016-11-10 15:27:27 +0800

    this is a addition

---
+++ app/src/main/java/com/example/administrator/test_github/Test.java
@@ -0,0 +1,15 @@
+package com.example.administrator.test_github;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/11/10 0010.
+ */
+
+public class Test extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
